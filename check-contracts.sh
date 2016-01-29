e44135440ec0d0d9ec199987fec3ae34bcd6738f
Dave Smith <david.smith5@itv.com>
2016-01-29 17:20:24 +0000

    Pact checker script now checks all pact files in the target dir.

--- check-contracts.sh
+++ check-contracts.sh
@@ -2,7 +2,7 @@
 
 # Initialisation
 PROJECT_ROOT_DIR=$(pwd)
-JSON_PACT_FILE_PATH="target/pacts/consumer-provider.json"
+JSON_PACT_FILE_PATH="$PROJECT_ROOT_DIR/target/pacts/*"
 PORT=1234
 BASE_URL="http://localhost:$PORT"
 
@@ -20,32 +20,43 @@ echo "Clear exisiting interactions"
 
 curl -X DELETE -H "X-Pact-Mock-Service: true" localhost:1234/interactions
 
-echo "Uploading interation"
-
-INTERACTIONS=$(cat $JSON_PACT_FILE_PATH | jq '.interactions')
-INTERACTION_COUNT=$(echo $INTERACTIONS | jq '. | length')
-
-while [ "$INTERACTION_COUNT" -gt 0 ]
+for f in $JSON_PACT_FILE_PATH
 do
-  INTERACTION_COUNT=$(($INTERACTION_COUNT - 1))
-  $(echo $INTERACTIONS | jq --compact-output .[$INTERACTION_COUNT] > tmp.json)
+  JSON_PACT_FILE=$f
 
-  curl -X POST \
-  -H "Content-Type: application/json" \
-  -H "X-Pact-Mock-Service: true" \
-  localhost:1234/interactions \
-  -d @tmp.json
+  echo "Uploading interation from file: $JSON_PACT_FILE"
 
-  rm tmp.json
+  INTERACTIONS=$(cat $JSON_PACT_FILE | jq '.interactions')
+  INTERACTION_COUNT=$(echo $INTERACTIONS | jq '. | length')
+
+  while [ "$INTERACTION_COUNT" -gt 0 ]
+  do
+    INTERACTION_COUNT=$(($INTERACTION_COUNT - 1))
+    $(echo $INTERACTIONS | jq --compact-output .[$INTERACTION_COUNT] > tmp.json)
+
+    curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "X-Pact-Mock-Service: true" \
+    localhost:1234/interactions \
+    -d @tmp.json
+
+    rm tmp.json
+  done
+
+  echo ""
+
 done
-
-echo ""
 
 # Step 3
 # Verify the PACT contracts against the stub service
 cd $PROJECT_ROOT_DIR/pact-verifier
-#bundle install
-endpoint=$BASE_URL spec_url="$PROJECT_ROOT_DIR/$JSON_PACT_FILE_PATH" bundle exec rake pact:verify
+bundle install
+
+for JSON_PACT_FILE in $JSON_PACT_FILE_PATH
+do
+  endpoint=$BASE_URL spec_url="$JSON_PACT_FILE" bundle exec rake pact:verify
+done
+
 
 # Kill the stub
 cd $PROJECT_ROOT_DIR
