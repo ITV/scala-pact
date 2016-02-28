98736ef487f8f3104ece4fe6fa1c3cd2c86c12db
Dave Smith <dave@moonfruit.com>
2016-02-28 01:15:06 +0000

    Added beginnings of stubber, just echos back what it was given.

--- scalapact-sbtplugin/project/Build.scala
+++ scalapact-sbtplugin/project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object Build extends sbt.Build with BuildExtra {
   lazy val sbtIdea = Project("scalapact-plugin", file("."), settings = mainSettings)
 
-  lazy val mainSettings: Seq[Def.Setting[_]] = /*Defaults.defaultSettings ++*/ Seq(
+  lazy val mainSettings: Seq[Def.Setting[_]] = Seq(
     sbtPlugin := true,
     organization := "com.itv.plugins",
     name := "scalapact-plugin",
@@ -13,7 +13,11 @@ object Build extends sbt.Build with BuildExtra {
     scalaVersion in Global := "2.10.6",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
     libraryDependencies ++= Seq(
-      "io.argonaut" %% "argonaut" % "6.1"
+      "io.argonaut" %% "argonaut" % "6.1",
+      "org.slf4j" % "slf4j-simple" % "1.6.4",
+      "org.http4s" %% "http4s-blaze-server" % "0.12.1",
+      "org.http4s" %% "http4s-dsl"          % "0.12.1",
+      "org.http4s" %% "http4s-argonaut"     % "0.12.1"
     )
   )
 }
--- scalapact-sbtplugin/src/main/scala/com/itv/plugin/ScalaPactStubberCommand.scala
+++ scalapact-sbtplugin/src/main/scala/com/itv/plugin/ScalaPactStubberCommand.scala
@@ -1,5 +1,10 @@
 package com.itv.plugin
 
+import argonaut.Argonaut._
+import org.http4s._
+import org.http4s.server._
+import org.http4s.dsl._
+import org.http4s.server.blaze.BlazeBuilder
 import sbt._
 
 object ScalaPactStubberCommand {
@@ -10,6 +15,49 @@ object ScalaPactStubberCommand {
 
     println("Placeholder for ScalaPact stubber command")
 
+    serverStart()
+      .awaitShutdown()
+
     state
+  }
+
+  private def serverStart(): Server = {
+    BlazeBuilder.bindHttp(1234)
+      .mountService(PactStubService.service, "/")
+      .run
+  }
+}
+
+object PactStubService {
+  val service = HttpService {
+
+    //TODO: Must have admin header
+    case GET -> Root / "interactions" =>
+      Ok("GET interactions")
+
+    //TODO: Must have admin header
+    case PUT -> Root / "interactions" =>
+      Ok("PUT interactions")
+
+    //TODO: Must have admin header
+    case POST -> Root / "interactions" =>
+      Ok("POST interactions")
+
+    //TODO: Must have admin header
+    case DELETE -> Root / "interactions" =>
+      Ok("DELETE interactions")
+
+    case req @ GET -> Root / path =>
+      Ok("GET " + req.pathInfo)
+
+    case req @ PUT -> Root / path =>
+      Ok("PUT " + req.pathInfo)
+
+    case req @ POST -> Root / path =>
+      Ok("POST " + req.pathInfo)
+
+    case req @ DELETE -> Root / path =>
+      Ok("DELETE " + req.pathInfo)
+
   }
 }
