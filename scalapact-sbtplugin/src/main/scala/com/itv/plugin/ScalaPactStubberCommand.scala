cd3e735a1c22a45b4ea626cae34ae048ee76f6ef
Dave Smith <dave@moonfruit.com>
2016-02-28 22:53:04 +0000

    Filling out the arg and file loading processes.

--- scalapact-sbtplugin/src/main/scala/com/itv/plugin/ScalaPactStubberCommand.scala
+++ scalapact-sbtplugin/src/main/scala/com/itv/plugin/ScalaPactStubberCommand.scala
@@ -1,11 +1,9 @@
 package com.itv.plugin
 
-import argonaut.Argonaut._
 import org.http4s._
-import org.http4s.server._
 import org.http4s.dsl._
-import org.http4s.util.CaseInsensitiveString
 import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.util.CaseInsensitiveString
 import sbt._
 
 object ScalaPactStubberCommand {
@@ -13,43 +11,29 @@ object ScalaPactStubberCommand {
   lazy val pactStubberCommandCamel: Command = Command.args("pactStubber", "<options>")(pactStubber)
 
   private lazy val pactStubber: (State, Seq[String]) => State = (state, args) => {
-
-    val config = parseArguments(args)
-
-    println("Starting ScalaPact Stubber on: http://" + config.host + ":" + config.port)
-
-    serverStart(config)
+    (StubArguments.parseArguments andThen LocalFileLoader.loadPactFiles andThen startServer)(args)
 
     state
   }
 
-  private def serverStart(config: Arguments): Unit = {
+  private lazy val startServer: Arguments => Unit = config => {
+    println("Starting ScalaPact Stubber on: http://" + config.host + ":" + config.port)
+
     BlazeBuilder.bindHttp(config.port, config.host)
       .mountService(PactStubService.service, "/")
       .run
       .awaitShutdown()
   }
 
-  private def safeStringToInt(s: String): Option[Int] = {
-    try {
-      Option(s.toInt)
-    } catch {
-      case e: Throwable => None
-    }
-  }
+}
 
-  private def parseArguments(args: Seq[String]): Arguments = {
-    val argMap = pair(args.toList)
+object StubArguments {
+  lazy val parseArguments: Seq[String] => Arguments = args =>
+    (pair andThen buildConfigMap)(args.toList)
 
-    Arguments(
-      host = argMap.getOrElse("--host", "localhost"),
-      port = argMap.get("--port").flatMap(safeStringToInt).getOrElse(1234)
-    )
-  }
-
-  def pair[A](list: List[A]): Map[A, A] = {
+  private lazy val pair: List[String] => Map[String, String] = list => {
     @annotation.tailrec
-    def rec(l: List[A], acc: List[Map[A, A]]): List[Map[A, A]] = {
+    def rec[A](l: List[A], acc: List[Map[A, A]]): List[Map[A, A]] = {
       l match {
         case Nil => acc
         case x :: Nil => acc
@@ -57,11 +41,96 @@ object ScalaPactStubberCommand {
       }
     }
 
-    rec(list, Nil).foldLeft(Map[A, A]())(_ ++ _)
+    rec(list, Nil).foldLeft(Map[String, String]())(_ ++ _)
   }
+
+  private lazy val buildConfigMap: Map[String, String] => Arguments = argMap =>
+    Arguments(
+      host = argMap.getOrElse("--host", "localhost"),
+      port = argMap.get("--port").flatMap(safeStringToInt).getOrElse(1234),
+      localPactPath = argMap.get("--source")
+    )
+
+  private lazy val safeStringToInt: String => Option[Int] = s =>
+    try {
+      Option(s.toInt)
+    } catch {
+      case e: Throwable => None
+    }
 }
 
-case class Arguments(host: String, port: Int)
+object LocalFileLoader {
+
+  private val recursiveJsonLoad: File => List[String] = path => {
+    @annotation.tailrec
+    def rec(files: List[File], acc: List[String]): List[String] = {
+      files match {
+        case Nil =>
+          acc
+
+        case x :: xs if !x.exists() =>
+          println("Supplied pact path does not exist! Aborting file load.")
+          Nil
+
+        case x :: xs if x.isDirectory =>
+          rec(x.listFiles().toList.filter(_.getName.endsWith(".json")) ++ files, acc)
+
+        case x :: xs if x.isFile =>
+          println("Loading pact file: " + x.getName)
+          rec(files, scala.io.Source.fromURL(x.toURI.toURL).getLines().mkString("\n") :: acc)
+
+        case _ =>
+          println("Aborting, problem reading the pact files at location: " + path.getCanonicalPath)
+          Nil
+      }
+    }
+
+    try {
+      rec(List(path).filter(_.getName.endsWith(".json")), Nil)
+    } catch {
+      case e: SecurityException =>
+        println("Did not have permission to access the provided path, message:\n" + e.getMessage)
+        Nil
+      case e: Throwable =>
+        println("Problem reading from supplied path, message:\n" + e.getMessage)
+        Nil
+    }
+  }
+
+  private val deserialiseIntoPact: List[String] => List[Pact] = pactJsonStrings => {
+    pactJsonStrings.map { json =>
+      //TODO: More nonsense
+      Pact("fish")
+    }
+  }
+
+  private val addToPactManager: List[Pact] => Unit = pacts => {
+    // TODO: What Pact manager?!?
+
+    ()
+  }
+
+  lazy val loadPactFiles: Arguments => Arguments = config => {
+    // Side effecting, might as well be since the pacts are held statefully /
+    // mutably so that they can be updated. The only way around this, I think,
+    // would be to start new servers on update? Or some sort of foldp to update
+    // the model?
+    config.localPactPath match {
+      case Some(path) =>
+        (recursiveJsonLoad andThen deserialiseIntoPact andThen addToPactManager) (new File(path))
+
+      case None => ()
+    }
+
+    config
+  }
+
+}
+
+// TODO: Clearly nonesense...
+case class Pact(id: String)
+
+case class Arguments(host: String, port: Int, localPactPath: Option[String])
 
 object PactStubService {
 
