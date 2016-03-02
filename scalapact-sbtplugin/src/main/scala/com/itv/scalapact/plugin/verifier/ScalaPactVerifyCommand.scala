6a3f04897b49903be0a0cf40df0e43d5d07b5585
Dave Smith <david.smith5@itv.com>
2016-03-02 18:15:59 +0000

    Added notes to the verify task explaining what to do.

--- scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/verifier/ScalaPactVerifyCommand.scala
+++ scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/verifier/ScalaPactVerifyCommand.scala
@@ -3,12 +3,22 @@ package com.itv.scalapact.plugin.verifier
 import sbt._
 
 object ScalaPactVerifyCommand {
+
+  //TODO: change to Command.args for a baseUrl and source dir argument
   lazy val pactVerifyCommandHyphen: Command = Command.command("pact-verify")(pactVerify)
   lazy val pactVerifyCommandCamel: Command = Command.command("pactVerify")(pactVerify)
 
   private lazy val pactVerify: State => State = state => {
 
     println("Placeholder for ScalaPact verification command")
+
+    // Load a bunch of pact files from a target dir
+
+    // Replay all requests against a baseUrl
+
+    // Match each response against the expected response
+
+    // Report
 
     state
   }
