ffc9a099ec016b60b84e55ed73fb633d1c44f122
Dave Smith <dave@moonfruit.com>
2016-03-04 00:12:27 +0000

    Goodbye Ruby.

--- scalapact-scalatest/pact-verifier/spec/helpers/pact_helper.rb
+++
@@ -1,8 +0,0 @@
-require_relative './provider_states'
-
-Pact.configure do |config|
-  config.log_dir     = 'spec/pact/log'
-  config.doc_dir     = 'spec/pact/doc'
-  config.reports_dir = 'spec/pact/reports'
-  config.tmp_dir     = 'spec/pact/tmp'
-end
--- scalapact-scalatest/pact-verifier/spec/helpers/provider_states.rb
+++
@@ -1,8 +0,0 @@
-require 'pact'
-
-Pact.provider_states_for 'ExampleConsumer' do
-  provider_state 'Provider service is available' do
-    set_up do
-    end
-  end
-end
