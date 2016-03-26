65fda384d0447544f5741b0edd559573481526d7
Dave Smith <dave@moonfruit.com>
2016-03-26 14:40:10 +0000

    Pact publisher should be done.

--- scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/publish/Publisher.scala
+++ scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/publish/Publisher.scala
@@ -4,20 +4,35 @@ import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
 import com.itv.scalapact.plugin.common.ConfigAndPacts
+import com.itv.scalapact.plugin.common.Rainbow._
+import com.itv.scalapactcore.ScalaPactWriter
+
+import scalaj.http.{Http, HttpResponse}
 
 object Publisher {
 
   lazy val publishToBroker: String => String => ConfigAndPacts => Unit = pactBrokerAddress => projectVersion => configAndPacts => {
 
+    val brokerAddress =
+      (configAndPacts.arguments.host, configAndPacts.arguments.port) match {
+        case (Some(h), None) if h.startsWith("http://") => h
+        case (Some(h), None) if h.contains(":") => "http://" + h
+        case (Some(h), Some(p)) => "http://" + h + ":" + p
+        case (None, None) => pactBrokerAddress
+      }
+
     configAndPacts.pacts.foreach { pact =>
 
-      //TODO: Fallbacks for host / port i.e. commandline -> sbt config -> ???
       //TODO: Snapshot version number handling?
-      //TODO: Publish using PUT
 
-      val address = pactBrokerAddress + "/pacts/provider/" + urlEncode(pact.provider.name) + "/consumer/" + urlEncode(pact.consumer.name) + "/version/" + projectVersion
+      val address = brokerAddress + "/pacts/provider/" + urlEncode(pact.provider.name) + "/consumer/" + urlEncode(pact.consumer.name) + "/version/" + projectVersion
 
-      println("Publish to: " + address)
+      println(s"Publishing to: $address".yellow)
+
+      Http(address).method("PUT").postData(ScalaPactWriter.pactToJsonString(pact)).asString match {
+        case r: HttpResponse[String] if r.is2xx => println("Success".green)
+        case r: HttpResponse[String] => println(s"Failed: ${r.body}".red)
+      }
 
     }
 
--- scalapact-scalatest/build.sbt
+++ scalapact-scalatest/build.sbt
@@ -14,7 +14,7 @@ libraryDependencies ++= Seq(
   "org.scalaj" %% "scalaj-http" % "1.1.5",
   "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
   "ch.qos.logback" % "logback-classic" % "1.0.3",
-  "com.itv" % "scalapact-core_2.11" % "0.1.3"
+  "com.itv" % "scalapact-core_2.11" % "0.1.4-SNAPSHOT"
 )
 
 publishTo := {
@@ -26,3 +26,5 @@ publishTo := {
 }
 
 resolvers += "Artifactory" at "https://itvrepos.artifactoryonline.com/itvrepos/oasvc-ivy/"
+
+pactBrokerAddress := "<nowhere>"
