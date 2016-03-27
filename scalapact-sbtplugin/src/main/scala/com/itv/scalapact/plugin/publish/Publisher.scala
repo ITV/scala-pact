87c184dbe3b0f8f1201c4f59d2296b8f524f9253
Dave Smith <dave@moonfruit.com>
2016-03-27 10:05:53 +0100

    Made pact broker address validation code reusable.

--- scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/publish/Publisher.scala
+++ scalapact-sbtplugin/src/main/scala/com/itv/scalapact/plugin/publish/Publisher.scala
@@ -3,35 +3,27 @@ package com.itv.scalapact.plugin.publish
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
-import com.itv.scalapact.plugin.common.ConfigAndPacts
+import com.itv.scalapact.plugin.common.{ConfigAndPacts, PactBrokerAddressValidation}
 import com.itv.scalapact.plugin.common.Rainbow._
 import com.itv.scalapactcore.ScalaPactWriter
 
 import scalaj.http.{Http, HttpResponse}
+import scalaz.{-\/, \/-}
 
 object Publisher {
 
   lazy val publishToBroker: String => String => ConfigAndPacts => Unit = pactBrokerAddress => projectVersion => configAndPacts => {
 
-    val urlMatch = "^(http)(s?)(://)([A-Za-z0-9-\\./]+)([A-Za-z0-9]$)"
+    PactBrokerAddressValidation.checkPactBrokerAddress(pactBrokerAddress) match {
+      case -\/(l) =>
+        println(l.red)
 
-    pactBrokerAddress match {
-      case a: String if a.isEmpty =>
-        println("Pact broker address not set, you need to add the following line to you SBT file: ".red)
-        println("pactBrokerAddress := \"http://pactbroker.myserver.com\"".red)
-
-      case a: String if !a.startsWith("http") =>
-        println("Pact broker address does not appear to be valid, should start with 'http(s)' protocol.".red)
-
-      case a: String if !a.matches(urlMatch) =>
-        println("Pact broker address does not appear to be valid, should be of form: http://my.broker-address.com".red)
-
-      case _ =>
+      case \/-(r) =>
         configAndPacts.pacts.foreach { pact =>
 
           //TODO: Snapshot version number handling? Find out if it works or not.
 
-          val address = pactBrokerAddress + "/pacts/provider/" + urlEncode(pact.provider.name) + "/consumer/" + urlEncode(pact.consumer.name) + "/version/" + projectVersion
+          val address = r + "/pacts/provider/" + urlEncode(pact.provider.name) + "/consumer/" + urlEncode(pact.consumer.name) + "/version/" + projectVersion
 
           println(s"Publishing to: $address".yellow)
 
