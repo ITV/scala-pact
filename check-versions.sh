52f755f0d55ca75cac894c7dd02aeec52193d691
Dave Smith <david.smith5@itv.com>
2016-10-04 15:38:44 +0100

    Improved local build script, extracted checking of versions.

---
+++ check-versions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+echo "Checking Scala-Pact Version Alignment"
+echo "*************************************"
+
+
+CORE_VERSION=$(grep "version :=" scalapact-core/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
+PLUGIN_VERSION=$(grep "version :=" scalapact-sbtplugin/project/Build.scala | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
+STANDALONE_VERSION=$(grep "version :=" scalapact-standalone-stubber/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
+TEST_VERSION=$(grep "version :=" scalapact-scalatest/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
+TEST_PLUGIN_VERSION=$(grep "scalapact-plugin" scalapact-scalatest/project/plugins.sbt | awk '{print $5}' | sed -e 's/[:space:,\")]//g')
+
+ALL_VERSIONS="$CORE_VERSION $PLUGIN_VERSION $STANDALONE_VERSION $TEST_VERSION $TEST_PLUGIN_VERSION"
+ALL_EXPECTED="$CORE_VERSION $CORE_VERSION $CORE_VERSION $CORE_VERSION $CORE_VERSION"
+
+if [[ $ALL_VERSIONS == $ALL_EXPECTED ]]; then
+  echo -e "Version set to $CORE_VERSION, proceed? [y/n] \c"
+  read CORRECT
+else
+  echo "Project versions did not match."
+  echo "Have you aligned the versions in all the projects? Found:"
+  echo "> core:                  $CORE_VERSION"
+  echo "> plugin:                $PLUGIN_VERSION"
+  echo "> standalone stubber:    $STANDALONE_VERSION"
+  echo "> test framework:        $TEST_VERSION"
+  echo "> test framework plugin: $TEST_PLUGIN_VERSION"
+  echo "Exiting, please fix the problem by aligning the versions."
+  exit 1
+fi
+
+if [ $CORRECT != 'y' ]; then
+  echo "Exiting"
+  exit 1
+else
+  echo "Continuing..."
+fi
--- local-build-test.sh
+++ local-build-test.sh
@@ -6,39 +6,9 @@
 set -e
 
 echo "Building and testing locally published Scala-Pact"
-echo "*************************************"
+echo "*************************************************"
 
-
-CORE_VERSION=$(grep "version :=" scalapact-core/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
-PLUGIN_VERSION=$(grep "version :=" scalapact-sbtplugin/project/Build.scala | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
-STANDALONE_VERSION=$(grep "version :=" scalapact-standalone-stubber/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
-TEST_VERSION=$(grep "version :=" scalapact-scalatest/build.sbt | sed 's/version :=//g' | sed 's/ //g' | sed 's/,//g' | sed 's/\"//g')
-TEST_PLUGIN_VERSION=$(grep "scalapact-plugin" scalapact-scalatest/project/plugins.sbt | awk '{print $5}' | sed -e 's/[:space:,\")]//g')
-
-ALL_VERSIONS="$CORE_VERSION $PLUGIN_VERSION $STANDALONE_VERSION $TEST_VERSION $TEST_PLUGIN_VERSION"
-ALL_EXPECTED="$CORE_VERSION $CORE_VERSION $CORE_VERSION $CORE_VERSION $CORE_VERSION"
-
-if [[ $ALL_VERSIONS == $ALL_EXPECTED ]]; then
-  echo "Version set to $CORE_VERSION, proceed? [y/n]"
-  read CORRECT
-else
-  echo "Project versions did not match."
-  echo "Have you aligned the versions in all the projects? Found:"
-  echo "> core:                  $CORE_VERSION"
-  echo "> plugin:                $PLUGIN_VERSION"
-  echo "> standalone stubber:    $STANDALONE_VERSION"
-  echo "> test framework:        $TEST_VERSION"
-  echo "> test framework plugin: $TEST_PLUGIN_VERSION"
-  echo "Exiting, please fix the problem by aligning the versions."
-  exit 1
-fi
-
-if [ $CORRECT != 'y' ]; then
-  echo "Exiting"
-  exit 1
-else
-  echo "Continuing..."
-fi
+bash check-versions.sh
 
 echo ""
 echo ">>> Core"
@@ -80,7 +50,7 @@ sbt "pact-stubber --port 1234" &
 
 COUNTDOWN=15
 
-echo "...giving the stubber a $COUNTDOWN head start to warm up..."
+echo "...giving the stubber a $COUNTDOWN second head start to warm up..."
 
 while [ $COUNTDOWN -ne 0 ]
 do
