7547e5a5d34d193358aa5aa6e76dc77b266bdd71
Dave Smith <dave@moonfruit.com>
2016-10-31 18:11:42 +0000

    Updated example delivery script.

--- example/deliver.sh
+++ example/deliver.sh
@@ -12,5 +12,23 @@ if [ ! -f $PACT_FILE ]
     exit 1
 fi
 
+PROVIDER_STUBBED_PATH="provider"
+PROVIDER_TESTS_PATH="provider_tests"
+
+if [ ! -d $PROVIDER_STUBBED_PATH ]
+  then
+    echo "Missing provider project at: $PROVIDER_STUBBED_PATH"
+    exit 1
+fi
+
+if [ ! -d $PROVIDER_TESTS_PATH ]
+  then
+    echo "Missing provider tests project at: $PROVIDER_TESTS_PATH"
+    exit 1
+fi
+
 mkdir -p provider/delivered_pacts
+cp $PACT_FILE provider/delivered_pacts/$PACT_NAME
+
+mkdir -p provider_tests/delivered_pacts
 cp $PACT_FILE provider/delivered_pacts/$PACT_NAME
