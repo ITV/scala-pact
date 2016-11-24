5262747860fe6bb518ce48c028588f8ec5bab24b
Dave Smith <david.smith5@itv.com>
2016-11-24 22:38:39 +0000

    Matching now uses a free monad. fully working but more can be done.

--- scalapact-core/src/main/scala/com/itv/scalapactcore/common/matching/InteractionMatchingPrograms.scala
+++ scalapact-core/src/main/scala/com/itv/scalapactcore/common/matching/InteractionMatchingPrograms.scala
@@ -1,8 +1,9 @@
 package com.itv.scalapactcore.common.matching
 
+import com.itv.scalapactcore.common.matching.PathMatching.PathAndQuery
+
 import scalaz._
 import Scalaz._
-
 import com.itv.scalapactcore.{InteractionRequest, InteractionResponse}
 
 sealed trait MatchStatusOutcome
@@ -25,16 +26,19 @@ sealed trait MatchPathOutcome
 case object MatchPathSuccess extends MatchPathOutcome
 case object MatchPathFailed extends MatchPathOutcome //TODO: What kind of match failures do we get that we can model?
 
-sealed trait MatchingStageA[A]
+sealed trait MatchResponseStageA[A]
 
-case class MatchResponseStatus(expected: InteractionResponse, actual: InteractionResponse) extends MatchingStageA[MatchStatusOutcome]
-case class MatchResponseHeaders(expected: InteractionResponse, actual: InteractionResponse) extends MatchingStageA[MatchHeadersOutcome]
-case class MatchResponseBody(expected: InteractionResponse, actual: InteractionResponse) extends MatchingStageA[MatchBodyOutcome]
+case class MatchResponseStatus(expected: InteractionResponse, actual: InteractionResponse) extends MatchResponseStageA[MatchStatusOutcome]
+case class MatchResponseHeaders(expected: InteractionResponse, actual: InteractionResponse) extends MatchResponseStageA[MatchHeadersOutcome]
+case class MatchResponseBody(expected: InteractionResponse, actual: InteractionResponse) extends MatchResponseStageA[MatchBodyOutcome]
 
-case class MatchRequestMethod(expected: InteractionRequest, actual: InteractionRequest) extends MatchingStageA[MatchMethodOutcome]
-case class MatchRequestPath(expected: InteractionRequest, actual: InteractionRequest) extends MatchingStageA[MatchPathOutcome]
-case class MatchRequestHeaders(expected: InteractionRequest, actual: InteractionRequest) extends MatchingStageA[MatchHeadersOutcome]
-case class MatchRequestBody(expected: InteractionRequest, actual: InteractionRequest) extends MatchingStageA[MatchBodyOutcome]
+
+sealed trait MatchRequestStageA[A]
+
+case class MatchRequestMethod(expected: InteractionRequest, actual: InteractionRequest) extends MatchRequestStageA[MatchMethodOutcome]
+case class MatchRequestPath(expected: InteractionRequest, actual: InteractionRequest) extends MatchRequestStageA[MatchPathOutcome]
+case class MatchRequestHeaders(expected: InteractionRequest, actual: InteractionRequest) extends MatchRequestStageA[MatchHeadersOutcome]
+case class MatchRequestBody(expected: InteractionRequest, actual: InteractionRequest) extends MatchRequestStageA[MatchBodyOutcome]
 
 case class RequestMatchOutcome(method: MatchMethodOutcome, path: MatchPathOutcome, headers: MatchHeadersOutcome, body: MatchBodyOutcome) {
   val success: Boolean = {
@@ -60,39 +64,41 @@ case class ResponseMatchOutcome(status: MatchStatusOutcome, headers: MatchHeader
 
 object InteractionMatchingPrograms {
 
-  type MatchingStage[A] = Free[MatchingStageA, A]
+  type MatchResponseStage[A] = Free[MatchResponseStageA, A]
 
   // Response
-  private def matchResponseStatus(expected: InteractionResponse, actual: InteractionResponse): MatchingStage[MatchStatusOutcome] =
+  private def matchResponseStatus(expected: InteractionResponse, actual: InteractionResponse): MatchResponseStage[MatchStatusOutcome] =
     Free.liftF(MatchResponseStatus(expected, actual))
 
-  private def matchResponseHeaders(expected: InteractionResponse, actual: InteractionResponse): MatchingStage[MatchHeadersOutcome] =
+  private def matchResponseHeaders(expected: InteractionResponse, actual: InteractionResponse): MatchResponseStage[MatchHeadersOutcome] =
     Free.liftF(MatchResponseHeaders(expected, actual))
 
-  private def matchResponseBody(expected: InteractionResponse, actual: InteractionResponse): MatchingStage[MatchBodyOutcome] =
+  private def matchResponseBody(expected: InteractionResponse, actual: InteractionResponse): MatchResponseStage[MatchBodyOutcome] =
     Free.liftF(MatchResponseBody(expected, actual))
 
-  def matchResponseProgram(expected: InteractionResponse, actual: InteractionResponse): MatchingStage[ResponseMatchOutcome] =
+  def matchResponseProgram(expected: InteractionResponse, actual: InteractionResponse): MatchResponseStage[ResponseMatchOutcome] =
     for {
       s <- matchResponseStatus(expected, actual)
       h <- matchResponseHeaders(expected, actual)
       b <- matchResponseBody(expected, actual)
     } yield ResponseMatchOutcome(s, h, b)
 
+  type MatchRequestStage[A] = Free[MatchRequestStageA, A]
+
   // Request
-  private def matchRequestMethod(expected: InteractionRequest, actual: InteractionRequest): MatchingStage[MatchMethodOutcome] =
+  private def matchRequestMethod(expected: InteractionRequest, actual: InteractionRequest): MatchRequestStage[MatchMethodOutcome] =
     Free.liftF(MatchRequestMethod(expected, actual))
 
-  private def matchRequestPath(expected: InteractionRequest, actual: InteractionRequest): MatchingStage[MatchPathOutcome] =
+  private def matchRequestPath(expected: InteractionRequest, actual: InteractionRequest): MatchRequestStage[MatchPathOutcome] =
     Free.liftF(MatchRequestPath(expected, actual))
 
-  private def matchRequestHeaders(expected: InteractionRequest, actual: InteractionRequest): MatchingStage[MatchHeadersOutcome] =
+  private def matchRequestHeaders(expected: InteractionRequest, actual: InteractionRequest): MatchRequestStage[MatchHeadersOutcome] =
     Free.liftF(MatchRequestHeaders(expected, actual))
 
-  private def matchRequestBody(expected: InteractionRequest, actual: InteractionRequest): MatchingStage[MatchBodyOutcome] =
+  private def matchRequestBody(expected: InteractionRequest, actual: InteractionRequest): MatchRequestStage[MatchBodyOutcome] =
     Free.liftF(MatchRequestBody(expected, actual))
 
-  def matchRequestProgram(expected: InteractionRequest, actual: InteractionRequest): MatchingStage[RequestMatchOutcome] =
+  def matchRequestProgram(expected: InteractionRequest, actual: InteractionRequest): MatchRequestStage[RequestMatchOutcome] =
     for {
       m <- matchRequestMethod(expected, actual)
       p <- matchRequestPath(expected, actual)
@@ -102,48 +108,106 @@ object InteractionMatchingPrograms {
 
 }
 
-object InteractionMatchingInterpreters {
+object MatchingInterpreters {
 
-  def permissiveResponseInterpreter: MatchingStageA ~> Id  =
-    new (MatchingStageA ~> Id) {
+  object Request {
 
-      def apply[A](fa: MatchingStageA[A]): Id[A] =
-        fa match {
-          case MatchResponseStatus(expected, actual) =>
-            if(StatusMatching.matchStatusCodes(expected.status)(actual.status)) MatchStatusSuccess.asInstanceOf[A]
-            else MatchStatusFailed.asInstanceOf[A]
+    object permissive extends (MatchRequestStageA ~> Id) {
 
-          case MatchResponseHeaders(expected, actual) =>
-            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess.asInstanceOf[A]
-            else MatchHeadersFailed.asInstanceOf[A]
+      def apply[A](stage: MatchRequestStageA[A]): Id[A] = {
+        stage match {
+          case MatchRequestMethod(expected, actual) =>
+            if(MethodMatching.matchMethods(expected.method)(actual.method)) MatchMethodSuccess
+            else MatchMethodFailed
 
-          case MatchResponseBody(expected, actual) =>
-            if(BodyMatching.matchBodies(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess.asInstanceOf[A]
-            else MatchBodyFailed.asInstanceOf[A]
+          case MatchRequestPath(expected, actual) =>
+            if(PathMatching.matchPaths(PathAndQuery(expected.path, expected.query))(PathAndQuery(actual.path, actual.query))) MatchPathSuccess
+            else MatchPathFailed
+
+          case MatchRequestHeaders(expected, actual) =>
+            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess
+            else MatchHeadersFailed
+
+          case MatchRequestBody(expected, actual) =>
+            if(BodyMatching.matchBodies(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess
+            else MatchBodyFailed
 
         }
+      }
 
     }
 
-  def strictResponseInterpreter: MatchingStageA ~> Id  =
-    new (MatchingStageA ~> Id) {
+    object strict extends (MatchRequestStageA ~> Id) {
 
-      def apply[A](fa: MatchingStageA[A]): Id[A] =
-        fa match {
-          case MatchResponseStatus(expected, actual) =>
-            if(StatusMatching.matchStatusCodes(expected.status)(actual.status)) MatchStatusSuccess.asInstanceOf[A]
-            else MatchStatusFailed.asInstanceOf[A]
+      def apply[A](stage: MatchRequestStageA[A]): Id[A] = {
+        stage match {
+          case MatchRequestMethod(expected, actual) =>
+            if(MethodMatching.matchMethods(expected.method)(actual.method)) MatchMethodSuccess
+            else MatchMethodFailed
 
-          case MatchResponseHeaders(expected, actual) =>
-            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess.asInstanceOf[A]
-            else MatchHeadersFailed.asInstanceOf[A]
+          case MatchRequestPath(expected, actual) =>
+            if(PathMatching.matchPathsStrict(PathAndQuery(expected.path, expected.query))(PathAndQuery(actual.path, actual.query))) MatchPathSuccess
+            else MatchPathFailed
 
-          case MatchResponseBody(expected, actual) =>
-            if(BodyMatching.matchBodiesStrict(true)(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess.asInstanceOf[A]
-            else MatchBodyFailed.asInstanceOf[A]
+          case MatchRequestHeaders(expected, actual) =>
+            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess
+            else MatchHeadersFailed
+
+          case MatchRequestBody(expected, actual) =>
+            if(BodyMatching.matchBodiesStrict(false)(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess
+            else MatchBodyFailed
 
         }
+      }
 
     }
+
+  }
+
+  object Response {
+
+    object permissive extends (MatchResponseStageA ~> Id) {
+
+      def apply[A](stage: MatchResponseStageA[A]): Id[A] = {
+        stage match {
+          case MatchResponseStatus(expected, actual) =>
+            if(StatusMatching.matchStatusCodes(expected.status)(actual.status)) MatchStatusSuccess
+            else MatchStatusFailed
+
+          case MatchResponseHeaders(expected, actual) =>
+            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess
+            else MatchHeadersFailed
+
+          case MatchResponseBody(expected, actual) =>
+            if(BodyMatching.matchBodies(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess
+            else MatchBodyFailed
+
+        }
+      }
+
+    }
+
+    object strict extends (MatchResponseStageA ~> Id) {
+
+      def apply[A](stage: MatchResponseStageA[A]): Id[A] = {
+        stage match {
+          case MatchResponseStatus(expected, actual) =>
+            if(StatusMatching.matchStatusCodes(expected.status)(actual.status)) MatchStatusSuccess
+            else MatchStatusFailed
+
+          case MatchResponseHeaders(expected, actual) =>
+            if(HeaderMatching.matchHeaders(expected.matchingRules)(expected.headers)(actual.headers)) MatchHeadersSuccess
+            else MatchHeadersFailed
+
+          case MatchResponseBody(expected, actual) =>
+            if(BodyMatching.matchBodiesStrict(true)(expected.matchingRules)(expected.body)(actual.body)) MatchBodySuccess
+            else MatchBodyFailed
+
+        }
+      }
+
+    }
+
+  }
 
 }
