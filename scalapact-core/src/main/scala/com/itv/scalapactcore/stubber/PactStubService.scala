f5b4047d931428e4a496a69a3271181bd02c54d2
Dave Smith <david.smith5@itv.com>
2016-12-15 17:15:12 +0000

    Removed thread sleep, fixed thread shared state bug, increase stub capacity.

--- scalapact-core/src/main/scala/com/itv/scalapactcore/stubber/PactStubService.scala
+++ scalapact-core/src/main/scala/com/itv/scalapactcore/stubber/PactStubService.scala
@@ -1,69 +1,79 @@
 package com.itv.scalapactcore.stubber
 
+import java.util.concurrent.{ExecutorService, Executors}
+
 import com.itv.scalapactcore._
-import com.itv.scalapactcore.common.{Arguments, Http4sRequestResponseFactory}
 import com.itv.scalapactcore.common.ColourOuput._
+import com.itv.scalapactcore.common.{Arguments, Http4sRequestResponseFactory}
 import org.http4s.dsl._
 import org.http4s.server.Server
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.util.CaseInsensitiveString
 import org.http4s.{HttpService, Request, Response, Status}
 
+import scala.concurrent.duration._
+
 object PactStubService {
 
-  lazy val startServer: Arguments => Unit = config => {
+  private val nThreads: Int = 50
+  private val executorService: ExecutorService = Executors.newFixedThreadPool(nThreads)
+
+  def startServer: Arguments => Unit = config => {
     println(("Starting ScalaPact Stubber on: http://" + config.giveHost + ":" + config.givePort).white.bold)
     println(("Strict matching mode: " + config.giveStrictMode).white.bold)
 
-    BlazeBuilder.bindHttp(config.givePort, config.giveHost)
-      .mountService(PactStubService.service(config.giveStrictMode), "/")
-      .run
-      .awaitShutdown()
+    runServer(new InteractionManager)(nThreads)(config).awaitShutdown()
   }
 
-  lazy val runServer: Arguments => Server = config => {
-    BlazeBuilder.bindHttp(config.givePort, config.giveHost)
-      .mountService(PactStubService.service(config.giveStrictMode), "/")
+  def runServer: InteractionManager => Int => Arguments => Server = interactionManager => connectionPoolSize => config => {
+    BlazeBuilder
+      .bindHttp(config.givePort, config.giveHost)
+      .withServiceExecutor(executorService)
+      .withIdleTimeout(60.seconds)
+      .withConnectorPoolSize(connectionPoolSize)
+      .mountService(PactStubService.service(interactionManager)(config.giveStrictMode), "/")
       .run
   }
 
-  lazy val stopServer: Server => Unit = server => {
+  def stopServer: Server => Unit = server => {
     server.shutdown
   }
 
   private val isAdminCall: Request => Boolean = request =>
-    request.pathInfo.startsWith("/interactions") &&
       request.headers.get(CaseInsensitiveString("X-Pact-Admin")).exists(h => h.value == "true")
 
-  private val service: Boolean => HttpService = strictMatching =>
+  private val service: InteractionManager => Boolean => HttpService = interactionManager => strictMatching =>
     HttpService.lift { req =>
-      matchRequestWithResponse(strictMatching)(req)
+      matchRequestWithResponse(interactionManager, strictMatching)(req)
     }
 
-  private def matchRequestWithResponse(strictMatching: Boolean)(req: Request): scalaz.concurrent.Task[Response] = {
+  private def matchRequestWithResponse(interactionManager: InteractionManager, strictMatching: Boolean)(req: Request): scalaz.concurrent.Task[Response] = {
     if(isAdminCall(req)) {
 
       req.method.name.toUpperCase match {
-        case m if m == "GET" =>
-          val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), InteractionManager.getInteractions))
+        case m if m == "GET" && req.pathInfo.startsWith("/stub/status") =>
+          Ok()
+
+        case m if m == "GET" && req.pathInfo.startsWith("/interactions") =>
+          val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), interactionManager.getInteractions))
           Ok(output)
 
-        case m if m == "POST" || m == "PUT" =>
+        case m if m == "POST" || m == "PUT" && req.pathInfo.startsWith("/interactions") =>
           ScalaPactReader.jsonStringToPact(req.bodyAsText.runLog.map(body => Option(body.foldLeft("")(_ + _))).unsafePerformSync.getOrElse("")) match {
             case Right(r) =>
-              InteractionManager.addInteractions(r.interactions)
+              interactionManager.addInteractions(r.interactions)
 
-              val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), InteractionManager.getInteractions))
+              val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), interactionManager.getInteractions))
               Ok(output)
 
             case Left(l) =>
               InternalServerError(l)
           }
 
-        case m if m == "DELETE" =>
-          InteractionManager.clearInteractions()
+        case m if m == "DELETE" && req.pathInfo.startsWith("/interactions") =>
+          interactionManager.clearInteractions()
 
-          val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), InteractionManager.getInteractions))
+          val output = ScalaPactWriter.pactToJsonString(Pact(PactActor(""), PactActor(""), interactionManager.getInteractions))
           Ok(output)
       }
 
@@ -72,7 +82,7 @@ object PactStubService {
 
       import com.itv.scalapactcore.common.HeaderImplicitConversions._
 
-      InteractionManager.findMatchingInteraction(
+      interactionManager.findMatchingInteraction(
         InteractionRequest(
           method = Option(req.method.name.toUpperCase),
           headers = Option(req.headers),
