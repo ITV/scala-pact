f64dda85d3195c2e04842f0c86f4d5aab3e2c2dd
Dave Smith <davesmith.00000@gmail.com>
2017-03-24 00:59:26 +0000

    Accept pact broker addresses with a port.

--- scalapact-core/src/main/scala/com/itv/scalapactcore/common/PactBrokerAddressValidation.scala
+++ scalapact-core/src/main/scala/com/itv/scalapactcore/common/PactBrokerAddressValidation.scala
@@ -2,7 +2,7 @@ package com.itv.scalapactcore.common
 
 object PactBrokerAddressValidation {
 
-  lazy val checkPactBrokerAddress: String => Either[String, String] = address =>
+  lazy val checkPactBrokerAddress: String => Either[String, ValidPactBrokerAddress] = address =>
     address match {
       case a: String if a.isEmpty =>
         Left("Pact broker address not set, you need to add the following line to you SBT file: \npactBrokerAddress := \"http://pactbroker.myserver.com\"")
@@ -10,11 +10,13 @@ object PactBrokerAddressValidation {
       case a: String if !a.startsWith("http") =>
         Left("Pact broker address does not appear to be valid, should start with 'http(s)' protocol.")
 
-      case a: String if !a.matches("^(http)(s?)(://)([A-Za-z0-9-\\./]+)([A-Za-z0-9]$)") =>
-        Left("Pact broker address does not appear to be valid, should be of form: http://my.broker-address.com")
+      case a: String if !a.matches("""^(http)(s?)(:\/\/)([A-Za-z0-9-\.\/]+)(:[0-9]+)?$""") =>
+        Left("Pact broker address does not appear to be valid, should be of form https://my.broker-address.com or http://localhost:8080")
 
       case a: String =>
-        Right(a)
+        Right(ValidPactBrokerAddress(a))
     }
 
 }
+
+case class ValidPactBrokerAddress(address: String)
