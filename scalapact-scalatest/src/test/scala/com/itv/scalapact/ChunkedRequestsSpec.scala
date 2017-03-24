38499bc3916f26183383d9eb6209d84c5897c68b
Dave Smith <david.smith5@itv.com>
2017-03-24 14:22:55 +0000

    Added an extra chunked request test.

--- scalapact-scalatest/src/test/scala/com/itv/scalapact/ChunkedRequestsSpec.scala
+++ scalapact-scalatest/src/test/scala/com/itv/scalapact/ChunkedRequestsSpec.scala
@@ -4,13 +4,12 @@ import com.itv.scalapact.ScalaPactForger._
 import org.scalatest.{FunSpec, Matchers}
 import fr.hmil.roshttp.{BackendConfig, HttpRequest}
 import monix.execution.Scheduler.Implicits.global
-import fr.hmil.roshttp.body.PlainTextBody
+import fr.hmil.roshttp.body.{BulkBodyPart, PlainTextBody}
+import java.nio.ByteBuffer
 
-import scala.util.{Failure, Success}
 import fr.hmil.roshttp.response.SimpleHttpResponse
 
 import scala.concurrent.duration._
-
 import scala.concurrent.Await
 
 /**
@@ -23,101 +22,6 @@ class ChunkedRequestsSpec extends FunSpec with Matchers {
     it("Should be able to accept a chunked request") {
 
       val endPoint = "/chunked"
-
-      val xml: String =
-        """<?xml version="1.0" encoding="UTF-8"?>
-          |<bookstore>
-          |  <book category="cooking">
-          |    <title lang="en">Everyday Italian</title>
-          |    <author>Giada De Laurentiis</author>
-          |    <year>2005</year>
-          |    <price>30.00</price>
-          |  </book>
-          |  <book category="children">
-          |    <title lang="en">Harry Potter</title>
-          |    <author>J K. Rowling</author>
-          |    <year>2005</year>
-          |    <price>29.99</price>
-          |  </book>
-          |  <book category="web">
-          |    <title lang="en">Learning XML</title>
-          |    <author>Erik T. Ray</author>
-          |    <year>2003</year>
-          |    <price>39.95</price>
-          |  </book>
-          |  <book category="cooking">
-          |    <title lang="en">Everyday Italian</title>
-          |    <author>Giada De Laurentiis</author>
-          |    <year>2005</year>
-          |    <price>30.00</price>
-          |  </book>
-          |  <book category="children">
-          |    <title lang="en">Harry Potter</title>
-          |    <author>J K. Rowling</author>
-          |    <year>2005</year>
-          |    <price>29.99</price>
-          |  </book>
-          |  <book category="web">
-          |    <title lang="en">Learning XML</title>
-          |    <author>Erik T. Ray</author>
-          |    <year>2003</year>
-          |    <price>39.95</price>
-          |  </book>
-          |  <book category="cooking">
-          |    <title lang="en">Everyday Italian</title>
-          |    <author>Giada De Laurentiis</author>
-          |    <year>2005</year>
-          |    <price>30.00</price>
-          |  </book>
-          |  <book category="children">
-          |    <title lang="en">Harry Potter</title>
-          |    <author>J K. Rowling</author>
-          |    <year>2005</year>
-          |    <price>29.99</price>
-          |  </book>
-          |  <book category="web">
-          |    <title lang="en">Learning XML</title>
-          |    <author>Erik T. Ray</author>
-          |    <year>2003</year>
-          |    <price>39.95</price>
-          |  </book>
-          |  <book category="cooking">
-          |    <title lang="en">Everyday Italian</title>
-          |    <author>Giada De Laurentiis</author>
-          |    <year>2005</year>
-          |    <price>30.00</price>
-          |  </book>
-          |  <book category="children">
-          |    <title lang="en">Harry Potter</title>
-          |    <author>J K. Rowling</author>
-          |    <year>2005</year>
-          |    <price>29.99</price>
-          |  </book>
-          |  <book category="web">
-          |    <title lang="en">Learning XML</title>
-          |    <author>Erik T. Ray</author>
-          |    <year>2003</year>
-          |    <price>39.95</price>
-          |  </book>
-          |  <book category="cooking">
-          |    <title lang="en">Everyday Italian</title>
-          |    <author>Giada De Laurentiis</author>
-          |    <year>2005</year>
-          |    <price>30.00</price>
-          |  </book>
-          |  <book category="children">
-          |    <title lang="en">Harry Potter</title>
-          |    <author>J K. Rowling</author>
-          |    <year>2005</year>
-          |    <price>29.99</price>
-          |  </book>
-          |  <book category="web">
-          |    <title lang="en">Learning XML</title>
-          |    <author>Erik T. Ray</author>
-          |    <year>2003</year>
-          |    <price>39.95</price>
-          |  </book>
-          |</bookstore>""".stripMargin
 
       forgePact
         .between("My Chunked Consumer")
@@ -150,7 +54,54 @@ class ChunkedRequestsSpec extends FunSpec with Matchers {
                 )
               )
 
-          val res = Await.result(request.post(PlainTextBody(xml)), 1.second)
+          val res: SimpleHttpResponse = Await.result(request.post(PlainTextBody(xml)), 1.second)
+
+          res.get.statusCode shouldEqual 200
+          res.get.body shouldEqual "Success"
+
+        }
+
+    }
+
+    it("should be able to accept a chunked request with realistic XML") {
+
+      val endPoint = "/chunked2"
+
+      forgePact
+        .between("My Chunked Consumer")
+        .and("Their Chunked Provider Service")
+        .addInteraction(
+          interaction
+            .description("another chunked request")
+            .uponReceiving(
+              method = POST,
+              path = endPoint,
+              query = None,
+              headers = Map("SOAPAction" -> "process", "Accept" -> "text/xml", "Content-Type" -> "text/xml; charset=utf-8", "User-Agent" -> "scala-pact-test"),
+              body = xml,
+              matchingRules = None
+            )
+            .willRespondWith(
+              status = 200,
+              headers = Map.empty,
+              body = "Success",
+              matchingRules = None
+            )
+        )
+        .runConsumerTest { mockConfig =>
+
+          val request =
+            HttpRequest()
+              .withBackendConfig(BackendConfig(
+                allowChunkedRequestBody = true
+              ))
+              .withURL(mockConfig.baseUrl)
+              .withPath(endPoint)
+              .withHeaders("SOAPAction" -> "process", "Accept" -> "text/xml", "Content-Type" -> "text/xml", "User-Agent" -> "scala-pact-test")
+              .withMethod(fr.hmil.roshttp.Method.POST)
+              .withBody(XmlBody(xml))
+
+          val res: SimpleHttpResponse = Await.result(request.send(), 1.second)
 
           res.get.statusCode shouldEqual 200
           res.get.body shouldEqual "Success"
@@ -161,4 +112,110 @@ class ChunkedRequestsSpec extends FunSpec with Matchers {
 
   }
 
+  def xml: String =
+    """<?xml version="1.0" encoding="UTF-8"?>
+      |<bookstore>
+      |  <book category="cooking">
+      |    <title lang="en">Everyday Italian</title>
+      |    <author>Giada De Laurentiis</author>
+      |    <year>2005</year>
+      |    <price>30.00</price>
+      |  </book>
+      |  <book category="children">
+      |    <title lang="en">Harry Potter</title>
+      |    <author>J K. Rowling</author>
+      |    <year>2005</year>
+      |    <price>29.99</price>
+      |  </book>
+      |  <book category="web">
+      |    <title lang="en">Learning XML</title>
+      |    <author>Erik T. Ray</author>
+      |    <year>2003</year>
+      |    <price>39.95</price>
+      |  </book>
+      |  <book category="cooking">
+      |    <title lang="en">Everyday Italian</title>
+      |    <author>Giada De Laurentiis</author>
+      |    <year>2005</year>
+      |    <price>30.00</price>
+      |  </book>
+      |  <book category="children">
+      |    <title lang="en">Harry Potter</title>
+      |    <author>J K. Rowling</author>
+      |    <year>2005</year>
+      |    <price>29.99</price>
+      |  </book>
+      |  <book category="web">
+      |    <title lang="en">Learning XML</title>
+      |    <author>Erik T. Ray</author>
+      |    <year>2003</year>
+      |    <price>39.95</price>
+      |  </book>
+      |  <book category="cooking">
+      |    <title lang="en">Everyday Italian</title>
+      |    <author>Giada De Laurentiis</author>
+      |    <year>2005</year>
+      |    <price>30.00</price>
+      |  </book>
+      |  <book category="children">
+      |    <title lang="en">Harry Potter</title>
+      |    <author>J K. Rowling</author>
+      |    <year>2005</year>
+      |    <price>29.99</price>
+      |  </book>
+      |  <book category="web">
+      |    <title lang="en">Learning XML</title>
+      |    <author>Erik T. Ray</author>
+      |    <year>2003</year>
+      |    <price>39.95</price>
+      |  </book>
+      |  <book category="cooking">
+      |    <title lang="en">Everyday Italian</title>
+      |    <author>Giada De Laurentiis</author>
+      |    <year>2005</year>
+      |    <price>30.00</price>
+      |  </book>
+      |  <book category="children">
+      |    <title lang="en">Harry Potter</title>
+      |    <author>J K. Rowling</author>
+      |    <year>2005</year>
+      |    <price>29.99</price>
+      |  </book>
+      |  <book category="web">
+      |    <title lang="en">Learning XML</title>
+      |    <author>Erik T. Ray</author>
+      |    <year>2003</year>
+      |    <price>39.95</price>
+      |  </book>
+      |  <book category="cooking">
+      |    <title lang="en">Everyday Italian</title>
+      |    <author>Giada De Laurentiis</author>
+      |    <year>2005</year>
+      |    <price>30.00</price>
+      |  </book>
+      |  <book category="children">
+      |    <title lang="en">Harry Potter</title>
+      |    <author>J K. Rowling</author>
+      |    <year>2005</year>
+      |    <price>29.99</price>
+      |  </book>
+      |  <book category="web">
+      |    <title lang="en">Learning XML</title>
+      |    <author>Erik T. Ray</author>
+      |    <year>2003</year>
+      |    <price>39.95</price>
+      |  </book>
+      |</bookstore>""".stripMargin
+
+}
+
+class XmlBody private(text: String, charset: String) extends BulkBodyPart {
+
+  override def contentType: String = "text/xml; charset=" + charset
+
+  override def contentData: ByteBuffer = ByteBuffer.wrap(text.getBytes())
+}
+
+object XmlBody {
+  def apply(text: String, charset: String = "utf-8"): XmlBody = new XmlBody(text, charset)
 }
