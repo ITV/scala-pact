311a1ee411a7fa6069a44019a9f0dfcb4f4d06ad
Dave Smith <david.j.smith@itv.com>
2017-07-31 15:39:57 +0100

    Updated regex to allow basic auth.

--- scalapact-core/src/test/scala/com/itv/scalapactcore/common/PactBrokerAddressValidationSpec.scala
+++ scalapact-core/src/test/scala/com/itv/scalapactcore/common/PactBrokerAddressValidationSpec.scala
@@ -10,7 +10,7 @@ class PactBrokerAddressValidationSpec extends FunSpec with Matchers {
 
       PactBrokerAddressValidation.checkPactBrokerAddress("") shouldEqual Left("Pact broker address not set, you need to add the following line to you SBT file: \npactBrokerAddress := \"http://pactbroker.myserver.com\"")
       PactBrokerAddressValidation.checkPactBrokerAddress("fish") shouldEqual Left("Pact broker address does not appear to be valid, should start with 'http(s)' protocol.")
-      PactBrokerAddressValidation.checkPactBrokerAddress("httpfish") shouldEqual Left("Pact broker address does not appear to be valid, should be of form https://my.broker-address.com or http://localhost:8080")
+      PactBrokerAddressValidation.checkPactBrokerAddress("httpfish") shouldEqual Left("Pact broker address does not appear to be valid, should be of form https://my.broker-address.com or http://localhost:8080 or http://user:pass@localhost:9000  or https://user:pass@my.broker-address.com ")
 
     }
 
@@ -21,6 +21,11 @@ class PactBrokerAddressValidationSpec extends FunSpec with Matchers {
 
     }
 
+    it("should accept ok addresses that have basic authentication") {
+      def checkOk(s: String) = PactBrokerAddressValidation.checkPactBrokerAddress(s) shouldEqual Right(ValidPactBrokerAddress(s))
+      checkOk("http://user:pass@localhost:5000")
+      checkOk("https://user:pass@my.broker-address.com")
+    }
   }
 
 }
