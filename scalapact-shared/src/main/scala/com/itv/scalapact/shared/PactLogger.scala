46bb48b186cceecd0b0d4d8d375d58dd7cf90f88
Dave Smith <david.j.smith@itv.com>
2018-02-16 17:22:13 +0000

    Added public type sigs, removed a couple of imports.

--- scalapact-shared/src/main/scala/com/itv/scalapact/shared/PactLogger.scala
+++ scalapact-shared/src/main/scala/com/itv/scalapact/shared/PactLogger.scala
@@ -1,41 +1,41 @@
 package com.itv.scalapact.shared
 
 trait PactLogger {
-  def message(s: Any)
-  def debug(s: Any)
-  def warn(s: Any)
-  def error(s: Any)
+  def message(s: Any): Unit
+  def debug(s: Any): Unit
+  def warn(s: Any): Unit
+  def error(s: Any): Unit
 }
 
 object NullPactLogger extends PactLogger {
-  def message(s: Any) = {}
-  def debug(s: Any) = {}
-  def warn(s: Any) = {}
-  def error(s: Any) = {}
+  def message(s: Any): Unit = ()
+  def debug(s: Any): Unit = ()
+  def warn(s: Any): Unit = ()
+  def error(s: Any): Unit = ()
 }
 object QuietPactLogger extends PactLogger {
-  def message(s: Any) = println(s)
-  def debug(s: Any) = {}
-  def warn(s: Any) =println(s)
-  def error(s: Any) =println(s)
+  def message(s: Any): Unit = println(s)
+  def debug(s: Any): Unit = {}
+  def warn(s: Any): Unit = println(s)
+  def error(s: Any): Unit = println(s)
 }
 
 object NoisyPactLogger extends PactLogger {
-  def message(s: Any) = println(s)
-  def debug(s: Any) = println(s)
-  def warn(s: Any) = println(s)
-  def error(s: Any) =println(s)
+  def message(s: Any): Unit = println(s)
+  def debug(s: Any): Unit = println(s)
+  def warn(s: Any): Unit = println(s)
+  def error(s: Any): Unit = println(s)
 }
 
 object PactLogger {
   private var logger: PactLogger = QuietPactLogger
 
-  def nullLOgger = logger = NullPactLogger
-  def quietLOgger = logger = QuietPactLogger
-  def noisyLOgger = logger = NoisyPactLogger
+  def nullLogger(): Unit = logger = NullPactLogger
+  def quietLogger(): Unit = logger = QuietPactLogger
+  def noisyLogger(): Unit = logger = NoisyPactLogger
 
-  def message(s: => String) =logger.message(s)
-  def debug(s: => String) = logger.debug(s)
-  def warn(s: => String) = logger.warn(s)
-  def error(s: => String) = logger.error(s)
+  def message(s: => String): Unit = logger.message(s)
+  def debug(s: => String): Unit = logger.debug(s)
+  def warn(s: => String): Unit = logger.warn(s)
+  def error(s: => String): Unit = logger.error(s)
 }
