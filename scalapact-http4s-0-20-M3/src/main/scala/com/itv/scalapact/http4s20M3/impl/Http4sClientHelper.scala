18b1301d461e50d9ec48f59364ef1c49e456dfa0
Andrew Ward <apbward@gmail.com>
2018-12-03 16:24:19 +0100

    Add Http4sClientHelper

---
+++ scalapact-http4s-0-20-M3/src/main/scala/com/itv/scalapact/http4s20M3/impl/Http4sClientHelper.scala
@@ -0,0 +1,47 @@
+package com.itv.scalapact.http4s20M3.impl
+
+import cats.effect.{IO, Resource}
+import com.itv.scalapact.shared.{SimpleRequest, SimpleResponse}
+import javax.net.ssl.SSLContext
+import org.http4s._
+import org.http4s.client.Client
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.headers.{AgentProduct, `User-Agent`}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+object Http4sClientHelper {
+
+  import HeaderImplicitConversions._
+
+  private val extractResponse: Response[IO] => IO[SimpleResponse] = r =>
+    r.bodyAsText.compile.toVector.map(_.mkString).map { b =>
+      SimpleResponse(r.status.code, r.headers, Some(b))
+  }
+
+  def defaultClient: Resource[IO, Client[IO]] =
+    buildPooledBlazeHttpClient(1, Duration(1, SECONDS), None)
+
+  def buildPooledBlazeHttpClient(maxTotalConnections: Int,
+                                 clientTimeout: Duration,
+                                 sslContext: Option[SSLContext]): Resource[IO, Client[IO]] = {
+    implicit val cs =  IO.contextShift(ExecutionContext.global)
+
+    BlazeClientBuilder[IO](ExecutionContext.global)
+      .withMaxTotalConnections(maxTotalConnections)
+      .withRequestTimeout(clientTimeout)
+      .withSslContextOption(sslContext)
+      .withUserAgentOption(Option(`User-Agent`(AgentProduct("scala-pact", Option(BuildInfo.version)))))
+      .withCheckEndpointAuthentication(false)
+      .resource
+  }
+
+  def doRequest(request: SimpleRequest, httpClient: IO[Client[IO]]): IO[SimpleResponse] =
+    for {
+      request  <- Http4sRequestResponseFactory.buildRequest(request)
+      client   <- httpClient
+      response <- client.fetch[SimpleResponse](request)(extractResponse)
+    } yield response
+
+}
