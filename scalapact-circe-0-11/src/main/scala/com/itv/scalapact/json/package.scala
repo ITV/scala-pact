e68b06b6f57640cc8e66d23ae4cd75f8ff8606fe
Martin Carolan <martin@carolan.email>
2019-02-11 16:02:35 +0000

    Support Circe 11 (v11.01)

---
+++ scalapact-circe-0-11/src/main/scala/com/itv/scalapact/json/package.scala
@@ -0,0 +1,10 @@
+package com.itv.scalapact
+
+import com.itv.scalapact.circe11.{PactReader, PactWriter}
+import com.itv.scalapact.shared.typeclasses.{IPactReader, IPactWriter}
+
+package object json {
+  implicit val pactReaderInstance: IPactReader = new PactReader
+
+  implicit val pactWriterInstance: IPactWriter = new PactWriter
+}
